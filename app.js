76e75211ab688ff24964310a65c1fcf10bf59bc1
danpeaku <daniel.sandoval@peaku.ai>
2022-11-09 13:24:01 -0500

    looping with v-for, loop through arrays and objects

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 new Vue({
   el: '#vue-app',
   data: {
-    error: false,
-    success: false
+    characters: ['Iori', 'Gato', 'Yashiro', "Yuri", "Kyo"],
+    ninjas: [
+      {name: 'Andy', age: 21},
+      {name: 'Eiji', age: 33},
+      {name: 'Mai', age: 22},
+    ]
   },
   methods: {
     
